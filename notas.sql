074262da84b7b9baf71bdd7207b85726d4c8aca7
José Manuel Martínez Caro (SDAW1) <josemanuel.martinez20@iesramonarcas.es>
2025-11-19 09:48:57 +0100

    Crear tablas de ejemplo del libro v2

--- notas.sql
+++ notas.sql
@@ -107,3 +107,101 @@ INSERT INTO ciudadanos (DNI_ciudadano, nombre_ciudadano, apellidos_ciudadano, te
             ('55555555E', 'Álvaro', 'Sánchez', 600000000, 5),
             ('66666666F', 'Francisco', 'Guerrero', 611111111, 6),
             ('77777777G', 'José', 'Peñarrieta', 622222222, 1);
+
+/* Se muestran los datos de la tabla ciudadanos*/
+SELECT * FROM ciudadanos;
+
+/*Mostrar las columnas DNI_ciudadano, nombre_ciudadano y telefono_ciudadano*/
+SELECT DNI_ciudadano, nombre_ciudadano, telefono_ciudadano FROM ciudadanos;
+
+SELECT  DNI_ciudadano AS 'DNI', 
+        nombre_ciudadano AS 'nombre', 
+        telefono_ciudadano AS 'telefono'
+FROM ciudadanos;
+
+
+/*AHORA VAMOS A PROCEDER CON LA BD clientesDB*/
+/* Crea una base de datos llamada clientesDB si no existe*/
+CREATE DATABASE IF NOT EXISTS clientesDB;
+
+/* Muestra las bases de datos existentes*/
+SHOW DATABASES;
+
+/*A partir de las bbdd existentes, selecciona clientesDB*/
+USE clientesDB;
+
+/* Crear las tablas existentes en esta base de datos */
+CREATE TABLE CustomersDaw (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(100),
+    ContactName VARCHAR(100),
+    Address VARCHAR(150),
+    City VARCHAR(100),
+    PostalCode VARCHAR(20),
+    Country VARCHAR(50)
+);
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(100),
+    ContactName VARCHAR(100),
+    Address VARCHAR(150),
+    City VARCHAR(100),
+    PostalCode VARCHAR(20),
+    Country VARCHAR(50)
+);
+
+/* Insertar datos en la tabla CustomersDaw */
+INSERT INTO CustomersDaw (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+(1, 'Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany'),
+(2, 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Avda. de la Constitución 2222', 'México D.F.', '05021', 'Mexico'),
+(3, 'Antonio Moreno Taquería', 'Antonio Moreno', 'Mataderos 2312', 'México D.F.', '05023', 'Mexico'),
+(4, 'Around the Horn', 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK'),
+(5, 'Berglunds snabbköp', 'Christina Berglund', 'Berguvsvägen 8', 'Luleå', 'S-958 22', 'Sweden');
+
+/* El INSERT INTO anterior es equivalente al siguiente INSERT INTO*/ 
+
+INSERT INTO CustomersDaw
+VALUES
+(1, 'Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany'),
+(2, 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Avda. de la Constitución 2222', 'México D.F.', '05021', 'Mexico'),
+(3, 'Antonio Moreno Taquería', 'Antonio Moreno', 'Mataderos 2312', 'México D.F.', '05023', 'Mexico'),
+(4, 'Around the Horn', 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK'),
+(5, 'Berglunds snabbköp', 'Christina Berglund', 'Berguvsvägen 8', 'Luleå', 'S-958 22', 'Sweden');
+
+/* Insertar datos en la tabla Customers */
+INSERT INTO Customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+(1, 'Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany'),
+(2, 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Avda. de la Constitución 2222', 'México D.F.', '05021', 'Mexico'),
+(3, 'Antonio Moreno Taquería', 'Antonio Moreno', 'Mataderos 2312', 'México D.F.', '05023', 'Mexico'),
+(4, 'Around the Horn', 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK'),
+(5, 'Berglunds snabbköp', 'Christina Berglund', 'Berguvsvägen 8', 'Luleå', 'S-958 22', 'Sweden'),
+(6, 'Blauer See Delikatessen', 'Hanna Moos', 'Forsterstr. 57', 'Mannheim', '68306', 'Germany'),
+(7, 'Blondel père et fils', 'Frédérique Citeaux', '24, place Kléber', 'Strasbourg', '67000', 'France'),
+(8, 'Bólido Comidas preparadas', 'Martín Sommer', 'C/ Araquil, 67', 'Madrid', '28023', 'Spain'),
+(9, 'Bon app’', 'Laurence Lebihans', '12, rue des Bouchers', 'Marseille', '13008', 'France'),
+(10, 'Bottom-Dollar Marketse', 'Elizabeth Lincoln', '23 Tsawassen Blvd.', 'Tsawassen', 'T2F 8M4', 'Canada'),
+(11, 'B''s Beverages', 'Victoria Ashworth', 'Fauntleroy Circus', 'London', 'EC2 5NT', 'UK'),
+(12, 'Cactus Comidas para llevar', 'Patricio Simpson', 'Cerrito 333', 'Buenos Aires', '1010', 'Argentina'),
+(13, 'Centro comercial Moctezuma', 'Francisco Chang', 'Sierras de Granada 9993', 'México D.F.', '05022', 'Mexico'),
+(14, 'Chop-suey Chinese', 'Yang Wang', 'Hauptstr. 29', 'Bern', '3012', 'Switzerland'),
+(15, 'Comércio Mineiro', 'Pedro Afonso', 'Av. dos Lusíadas, 23', 'São Paulo', '05432-043', 'Brazil'),
+(16, 'Consolidated Holdings', 'Elizabeth Brown', 'Berkeley Gardens 12 Brewery', 'London', 'WX1 6LT', 'UK'),
+(17, 'Drachenblut Delikatessend', 'Sven Ottlieb', 'Walserweg 21', 'Aachen', '52066', 'Germany'),
+(18, 'Du monde entier', 'Janine Labrune', '67, rue des Cinquante Otages', 'Nantes', '44000', 'France'),
+(19, 'Eastern Connection', 'Ann Devon', '35 King George', 'London', 'WX3 6FW', 'UK'),
+(20, 'Ernst Handel', 'Roland Mendel', 'Kirchgasse 6', 'Graz', '8010', 'Austria');
+
+
+/*Mostrar los datos en la tabla CustomersDaw*/
+SELECT * FROM CustomersDaw;
+
+/*Mostrar los datos en la tabla Customers*/
+SELECT * FROM Customers;
+
+/*Devolver una tabla que solo devuelva el Nombre del cliente y la ciudad de la tabla CustomersDaw*/
+SELECT CustomerName, City FROM CustomersDaw;
+
+/*Mostrar las ciudades difernetes que hay en la tabla Customers*/
